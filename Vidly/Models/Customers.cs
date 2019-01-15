933198438b841ad4a8c0f282b009e66602fab88c
ManeshMangrani <manesh.mangrani@gmail.com>
2019-01-15 22:43:39 +0500

    Uploaded

---
+++ Vidly/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customers
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name="MembershipType")]
+        public byte MembershipTypesId { get; set; }
+
+        [Display(Name="Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+
+
+    }
+}
